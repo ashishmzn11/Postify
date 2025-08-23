66b5e263acd1dd33d89d280d88f396ea1da2d952
ashishmzn11 <ashishmzn11@gmail.com>
2025-08-23 14:55:08 +0530

    second commit

---
+++ src/conf/conf.js
@@ -0,0 +1,9 @@
+const conf={
+  appwriteurl:String(mport.meta.env.VITE_APPWRITE_URL),
+  appwriteprojectid:String(mport.meta.env.VITE_APPWRITE_PROJECT_ID),
+  appwritedatabaseid:String(mport.meta.env.VITE_APPWRITE_DATABASE_ID),
+  appwritecollectionid:String(mport.meta.env.VITE_APPWRITE_COLLECTION_ID),
+  appwritebucketid:String(mport.meta.env.VITE_APPWRITE_BUCKET_ID),
+
+}
+export default conf;

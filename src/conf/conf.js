27afebf5bbbf42909f7f455dc30d8b1ecb845684
ashishmzn11 <ashishmzn11@gmail.com>
2025-08-25 16:58:47 +0530

    therd commit

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -1,9 +1,10 @@
-const conf={
-  appwriteurl:String(mport.meta.env.VITE_APPWRITE_URL),
-  appwriteprojectid:String(mport.meta.env.VITE_APPWRITE_PROJECT_ID),
-  appwritedatabaseid:String(mport.meta.env.VITE_APPWRITE_DATABASE_ID),
-  appwritecollectionid:String(mport.meta.env.VITE_APPWRITE_COLLECTION_ID),
-  appwritebucketid:String(mport.meta.env.VITE_APPWRITE_BUCKET_ID),
-
+const conf = {
+    appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
 }
-export default conf;
+// there was a name issue with the import.meta.env.VITE_APPWRITE_URL, it was later fixed in debugging video
+
+export default conf
